937888daa7d62d5900f945f2ba811759c70bb08c
alalan55 <alla.ao4975@gmail.com>
2024-06-10 18:31:22 -0300

    Startup

---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>Sou a home funcionando legalll</h1>
+    <v-btn @click="$router.push('/checkout')">ir para checkout</v-btn>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped></style>
